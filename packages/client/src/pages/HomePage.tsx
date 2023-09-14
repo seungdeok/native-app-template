c057c5857c4c4c4c42aa30d0e518c4ad3dd6a13f
seungdeok <akk9132@naver.com>
2023-09-14 22:02:14 +0900

    feat(client): Add client routing

--- packages/client/src/pages/HomePage.tsx
+++ packages/client/src/pages/HomePage.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 export function HomePage() {
   return <div>HOME PAGE</div>;
 }
--- packages/client/src/views/Home.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export function HomePage() {
-  return <div>HOME PAGE</div>;
-}
